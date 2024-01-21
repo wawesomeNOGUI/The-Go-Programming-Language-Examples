5decbee99aacce2c4d41a7f426e50149268f02e7
Will Forcey <wsforc3y@gmail.com>
2024-01-20 22:32:17 -0500

    Add files via upload

--- ch1/1.7/ex1.12.go
+++ ch1/1.7/ex1.12.go
@@ -17,6 +17,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -28,10 +30,11 @@ func main() {
 
 // handler sends a lissajous gif to http client
 // and takes input from the url to control the generation of the lissajous figure
+// input should be seperated by semicolons
 func handler(w http.ResponseWriter, r *http.Request) {
 	// default gen parameters
 	var (
-		cycles  = 5.0     // number of complete x oscillator revolutions
+		cycles  = 5.0   // number of complete x oscillator revolutions
 		res     = 0.001 // angular resolution
 		size    = 200   // image canvas covers [-size..+size]
 		nframes = 64    // number of animation frames
@@ -39,18 +42,82 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	)
 	var palette = []color.Color{color.Black, color.RGBA{0x00, 0xFF, 0x00, 0xFF}}
 
-	// get url
-	fmt.Println(r.URL.String())
+	// get url parameters and remove forward slash
+	url := r.URL.String()
+	url = url[1:]
 
+	params := strings.Split(url, ";")
+	for _, v := range params {
+		// ignore last semicolon if no command following it
+		if v == "" {
+			break
+		}
+
+		// remove whitespace and get this parameters name and value
+		// (wrl sends spaces as "%20")
+		vTrimmed := strings.ReplaceAll(v, "%20", "")
+		p := strings.Split(vTrimmed, "=")
+
+		// check for invalid syntax of command (should only have two elements: [name, value])
+		if len(p) != 2 {
+			fmt.Fprintln(w, "Invalid syntax!: ", strings.ReplaceAll(v, "%20", " "))
+			return
+		}
+
+		fmt.Println(p)
+
+		switch p[0] {
+		case "cycles":
+			tmp, err := strconv.ParseFloat(p[1], 64)
+			if err == strconv.ErrSyntax {
+				fmt.Fprintln(w, "Invalid syntax, value must be float64: ", strings.ReplaceAll(v, "%20", " "))
+				return
+			} else {
+				cycles = tmp
+			}
+		case "res":
+			tmp, err := strconv.ParseFloat(p[1], 64)
+			if err == strconv.ErrSyntax {
+				fmt.Fprintln(w, "Invalid syntax, value must be float64: ", strings.ReplaceAll(v, "%20", " "))
+				return
+			} else {
+				res = tmp
+			}
+		case "size":
+			tmp, err := strconv.Atoi(p[1])
+			if err != nil {
+				fmt.Fprintln(w, "Invalid syntax, value must be integer: ", strings.ReplaceAll(v, "%20", " "))
+				return
+			} else {
+				size = tmp
+			}
+		case "nframes":
+			tmp, err := strconv.Atoi(p[1])
+			if err != nil {
+				fmt.Fprintln(w, "Invalid syntax, value must be integer: ", strings.ReplaceAll(v, "%20", " "))
+				return
+			} else {
+				nframes = tmp
+			}
+		case "delay":
+			tmp, err := strconv.Atoi(p[1])
+			if err != nil {
+				fmt.Fprintln(w, "Invalid syntax, value must be integer: ", strings.ReplaceAll(v, "%20", " "))
+				return
+			} else {
+				delay = tmp
+			}
+		}
+	}
 
 	lissajous(w, cycles, res, size, nframes, delay, palette)
 }
 
 // lissajous generates lissajous gif and writes it to out
 func lissajous(out io.Writer, cycles, res float64, size, nframes, delay int, palette color.Palette) {
-	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
-	anim := gif.GIF{LoopCount: nframes}
-	phase := 0.0 // phase difference
+	freq := rand.Float64() * 3.0  // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: 0} // loop forever
+	phase := 0.0                  // phase difference
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
