ebe3301a93cb8236d732565d930120cfcb12c7ce
Will Forcey <wsforc3y@gmail.com>
2024-01-30 12:48:59 -0500

    update comment

--- ch1/1.7/ex1.12.go
+++ ch1/1.7/ex1.12.go
@@ -1,9 +1,6 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
 // See page 21.
 
-// Server3 is an "echo" server that displays request parameters.
+// Sends a lissajous gif according to optional URL arguments
 package main
 
 import (
